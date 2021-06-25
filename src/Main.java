b7fcdfef190c2850aa1ba671f928f8b41629b37f
vandivici <kangys1245@gmail.com>
2021-06-25 19:50:59 +0900

    Upload prj

---
+++ src/Main.java
@@ -0,0 +1,127 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+    public static char[][] MAP;
+    public static char user;
+    public static char ai;
+
+    public static void main(String[] args) throws IOException {
+        int turn = 1;
+        init();
+
+        Node TicTacToeAI;
+        if(user == 'O') {
+            TicTacToeAI = new Node(MAP, 0, ai);
+        }
+        else{
+            TicTacToeAI = new Node(MAP, 0, user);
+        }
+        // 제너레이터가 O를 받으면 작동안함
+        // user로 X를 받으면 잘 작동함. 해결할것
+        Node.generatePossibleAction(TicTacToeAI,ai);
+
+        for(Node node : TicTacToeAI.possibleAction) {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    System.out.printf("%c ",node.map[i][j]);
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+
+        char currTurn = user == 'O' ? user : ai;
+//        while(turn < 9){
+//            renderMap();
+//            int r;
+//            int c;
+//            if(currTurn == user) {
+//                byte[] input = new byte[3];
+//                while(true) {
+//                    System.out.println("Choose coordinate. ex) `2 1`");
+//                    System.in.read(input,0,3);
+//                    r = new String(input).charAt(0) - '0';
+//                    c = new String(input).charAt(2)- '0';
+//                    if(r > -1 && c > -1 && r < 3 && c < 3) {
+//                        currTurn = ai;
+//                        break;
+//                    }
+//                    System.out.println("Invalid input as r: "+ r + " c: " + c);
+//                }
+//                MAP[r][c] = user;
+//                // ai 업데이트안됨
+//                for(Node next : TicTacToeAI.possibleAction) {
+//                    System.out.println("DEBUG: "+next.map[r][c]);
+//                    if(next.map[r][c] != 0) {
+//                        System.out.println("DEBUG");
+//                        TicTacToeAI = next;
+//                        break;
+//                    }
+//                }
+//            }else {
+//                int nextPoint = Node.getBestAction(TicTacToeAI,ai);
+//                for(Node next : TicTacToeAI.possibleAction) {
+//                    if(next.point == nextPoint) {
+//                        TicTacToeAI = next;
+//                        Node.deepCpyMap(MAP,TicTacToeAI.map);
+//                        currTurn = user;
+//                        break;
+//                    }
+//                }
+//                // ai
+//            }
+//            turn++;
+//            clearConsole();
+//        }
+        // 이 인자대로라면 첫번쨰 수는 'O' 이어야 하는데 재귀때문에 한번 바뀌면서 `X`가 첫수
+        // 사용자에게 첫수 입력받으면 한번 돌려서 인자로 줘야할듯
+
+//        Node curr = TicTacToeAI;
+//        while(true) {
+//            System.out.println("Curr depth: "+curr.depth);
+//            System.out.println("Curr Point: "+curr.point);
+//            for(int i=0; i<3; i++) {
+//                for(int j=0; j<3; j++) {
+//                    System.out.printf("%c ", curr.map[i][j]);
+//                }
+//                System.out.println();
+//            }
+//            System.out.println();
+//            if(curr.possibleAction.size() == 0) {
+//                break;
+//            }
+//            curr = curr.possibleAction.get(0);
+//        }
+    }
+
+    public static void clearConsole() throws IOException {
+        final String ANSI_CLS = "\u001b[2J";
+        final String ANSI_HOME = "\u001b[H";
+        System.out.print(ANSI_CLS + ANSI_HOME);
+        System.out.flush();
+    }
+
+    public static void init() throws IOException {
+        MAP = new char[3][3];
+        byte[] input = new byte[1];
+        while(true) {
+            System.out.println("Choose player: `O` or `X`");
+            System.in.read(input,0,1);
+            user = new String(input).charAt(0);
+            if(user == 'O' || user == 'X') {
+                clearConsole();
+                return;
+            }
+        }
+    }
+
+    public static void renderMap() {
+        for(int i=0; i<3; i++) {
+            for(int j=0; j<3; j++) {
+                System.out.printf("%c ", MAP[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Node.java
@@ -0,0 +1,118 @@
+import java.util.ArrayList;
+
+public class Node {
+    public char[][] map;
+    public int depth;
+    public char turn;
+    public ArrayList<Node> possibleAction;
+    public int point;
+    public static int dr[] = {0, -1, -1, -1, 0, 1, 1, 1};
+    public static int dc[] = {1, 1, 0, -1, -1, -1, 0, 1};
+    public int chooseR;
+    public int chooseC;
+
+    public Node(char[][] currMap, int currDepth, char currTurn) {
+        map = new char[3][3];
+        deepCpyMap(map, currMap);
+        depth = currDepth;
+        turn = currTurn;
+        possibleAction = new ArrayList<>();
+        point = 0;
+    }
+
+    public static void deepCpyMap(char[][] mapDest, char[][] mapSrc) {
+        for (int i = 0; i < 3; i++) {
+            System.arraycopy(mapSrc[i], 0, mapDest[i], 0, 3);
+        }
+    }
+
+    public static void generatePossibleAction(Node node, char ai) {
+        if (node.depth == 10) {
+            return;
+        }
+        // 승부가 갈렸다면 그 이후의 수를 볼 필요가 없다. 종료
+        if (node.point != 0) {
+            return;
+        }
+        char currTurn = node.turn == 'O' ? 'X' : 'O';
+
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (node.map[r][c] == 0) {
+                    Node newPossibleAction = new Node(node.map, node.depth + 1, currTurn);
+                    node.chooseR = r;
+                    node.chooseC = c;
+                    newPossibleAction.map[r][c] = node.turn;
+                    setPoint(newPossibleAction, ai);
+                    node.possibleAction.add(newPossibleAction);
+                }
+            }
+        }
+
+        for (Node nextAction : node.possibleAction) {
+            generatePossibleAction(nextAction, ai);
+        }
+    }
+
+    public static char evaluateMap(char[][] map) {
+        char ret = 0;
+        // 맵 전체를 돕니다.
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                // 만약 수가 놓여진 공간일 때
+                if (map[i][j] != 0) {
+                    int r;
+                    int c;
+                    // 8방향으로 일치하는게 있는지 쭉 둘러보고
+                    for (int currDiff = 0; currDiff < 8; currDiff++) {
+                        int cnt = 0;
+                        r = i + dr[currDiff];
+                        c = j + dc[currDiff];
+                        // 배열 안벗어나는지 검사 및 승리조건 만족 검사하면서
+                        while (r > -1 && c > -1 && r < 3 && c < 3 && map[r][c] == map[i][j]) {
+                            cnt++;
+                            // 3줄로 동일한게 완성되면 빙고!
+                            if (cnt == 2) {
+                                return map[r][c];
+                            }
+                            // r = i + dr[currDiff]; 로 잘못썼었음
+                            // 이럴경우 2개만 빙고여도 점수받음. 조심하자
+                            r = r + dr[currDiff];
+                            c = c + dc[currDiff];
+                        }
+                    }
+                }
+            }
+        }
+        // 만약 빙고가 안나오면 0을 리턴
+        return ret;
+    }
+
+    public static void setPoint(Node node, char ai) {
+        char evaluation = evaluateMap(node.map);
+        if (evaluation == 0) {
+            node.point = 0;
+        } else if (evaluation == ai) {
+            node.point = 10;
+        } else {
+            node.point = -10;
+        }
+    }
+
+    public static int getBestAction(Node node, char turn) {
+        if(node.turn == turn) {
+            int bestPoint = -10;
+            for(Node nextAction : node.possibleAction) {
+                bestPoint = Integer.max(bestPoint,getBestAction(nextAction,turn));
+            }
+            node.point = bestPoint;
+            return bestPoint;
+        }
+        int worstPoint = 10;
+        for(Node nextAction : node.possibleAction) {
+            worstPoint = Integer.max(worstPoint,getBestAction(nextAction,turn));
+        }
+        node.point = worstPoint;
+        return worstPoint;
+    }
+}
